d432a057571c913545a9a4bf2a1147cae7a18e18
Ann <anaykanaeva@yandex.ru>
2022-07-07 20:33:15 +0300

    HW Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите первое число:");
+string first = Console.ReadLine();
+int firstnumber = int.Parse(first);
+Console.WriteLine("Введите второе число:");
+string second = Console.ReadLine();
+int secondnumber = int.Parse(second);
+Console.WriteLine("Введите третье число:");
+string third = Console.ReadLine();
+int thirdnumber = int.Parse(third);
+
+int max = firstnumber;
+
+if (secondnumber>max) max = secondnumber;
+if (thirdnumber>max) max = thirdnumber;
+Console.Write("max=");
+Console.WriteLine(max);
+
+
+
+
